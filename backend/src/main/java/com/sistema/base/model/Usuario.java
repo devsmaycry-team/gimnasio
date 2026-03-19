bfe71bf02560e57f052ec9b67f68ce1d81a83b54
Yamil Nicolas Ruffa <yamilruffa@gmail.com>
2026-03-19 13:49:14 -0300

    commit yamil

--- backend/src/main/java/com/sistema/base/model/Usuario.java
+++ backend/src/main/java/com/sistema/base/model/Usuario.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -18,120 +19,62 @@ import jakarta.persistence.OneToMany;
 @Entity
 public class Usuario {
     @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @ManyToOne
     @JoinColumn(name = "persona_id")
-	public Persona persona;
+    public Persona persona;
 
-	private String correo;
-	private String password;
+    private String correo;
+    private String password;
     private Boolean activo = false;
     private String verificationToken; 
     private String resetPasswordToken;
     private LocalDateTime resetPasswordTokenExpira;
-
     private LocalDateTime verificationTokenExpira;
+
+    @JsonIgnore
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonBackReference("UserRol-user")
     private List<UserRol> userRols = new ArrayList<>();
 
+    @JsonIgnore
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
     private List<Socio> socios;
     
-    public Usuario(){
+    public Usuario() {}
 
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
+    public Persona getPersona() { return persona; }
+    public void setPersona(Persona persona) { this.persona = persona; }
 
-    public Long getId() {
-        return id;
-    }
-    
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
 
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
-    public Persona getPersona() {
-        return persona;
-    }
+    public Boolean getActivo() { return activo; }
+    public void setActivo(Boolean activo) { this.activo = activo; }
 
-    public void setPersona(Persona persona) {
-        this.persona = persona;
-    }
+    public List<UserRol> getUserRols() { return userRols; }
+    public void setUserRols(List<UserRol> userRols) { this.userRols = userRols; }
 
-    public String getCorreo() {
-        return correo;
-    }
+    public String getVerificationToken() { return verificationToken; }
+    public void setVerificationToken(String verificationToken) { this.verificationToken = verificationToken; }
 
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
+    public LocalDateTime getVerificationTokenExpira() { return verificationTokenExpira; }
+    public void setVerificationTokenExpira(LocalDateTime verificationTokenExpira) { this.verificationTokenExpira = verificationTokenExpira; }
 
-    public String getPassword() {
-        return password;
-    }
+    public List<Socio> getSocios() { return socios; }
+    public void setSocios(List<Socio> socios) { this.socios = socios; }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getResetPasswordToken() { return resetPasswordToken; }
+    public void setResetPasswordToken(String resetPasswordToken) { this.resetPasswordToken = resetPasswordToken; }
 
-    public Boolean getActivo() {
-        return activo;
-    }
-
-    public void setActivo(Boolean activo) {
-        this.activo = activo;
-    }
-
-    public List<UserRol> getUserRols() {
-        return userRols;
-    }
-
-    public void setUserRols(List<UserRol> userRols) {
-        this.userRols = userRols;
-    }
-
-    public String getVerificationToken() {
-        return verificationToken;
-    }
-
-    public void setVerificationToken(String verificationToken) {
-        this.verificationToken = verificationToken;
-    }
-
-    public LocalDateTime getVerificationTokenExpira() {
-        return verificationTokenExpira;
-    }
-
-    public void setVerificationTokenExpira(LocalDateTime verificationTokenExpira) {
-        this.verificationTokenExpira = verificationTokenExpira;
-    }
-
-    public List<Socio> getSocios() {
-        return socios;
-    }
-
-    public void setSocios(List<Socio> socios) {
-        this.socios = socios;
-    }
-
-    public String getResetPasswordToken() {
-        return resetPasswordToken;
-    }
-
-    public void setResetPasswordToken(String resetPasswordToken) {
-        this.resetPasswordToken = resetPasswordToken;
-    }
-
-    public LocalDateTime getResetPasswordTokenExpira() {
-        return resetPasswordTokenExpira;
-    }
-
-    public void setResetPasswordTokenExpira(LocalDateTime resetPasswordTokenExpira) {
-        this.resetPasswordTokenExpira = resetPasswordTokenExpira;
-    }
-    
+    public LocalDateTime getResetPasswordTokenExpira() { return resetPasswordTokenExpira; }
+    public void setResetPasswordTokenExpira(LocalDateTime resetPasswordTokenExpira) { this.resetPasswordTokenExpira = resetPasswordTokenExpira; }
 }
